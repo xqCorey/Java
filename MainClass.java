10dcc3735d2c327171ac711d2eaf1798a03c852c
Corey <87344090+xqCorey@users.noreply.github.com>
2023-11-11 17:42:53 +0000

    Create MainClass.java

---
+++ MainClass.java
@@ -0,0 +1,13 @@
+
+public class MainClass {
+	public static void main(String[] args)
+	{
+		Employee test = new Employee("Corey", 50.0, "23.01.2000");
+		Manager test1 = new Manager("Euan", 60.0, "06.01.1998", "StockRoom");
+		test.getDetails();
+		test1.getDetails();
+	}
+	
+}
+
+//Signed by Corey Y!
